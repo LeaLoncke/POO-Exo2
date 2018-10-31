848d978a1c1b1e4e8101a8f99aa50f6dd6b16467
Lea Loncke <lealoncke1204@gmail.com>
2018-10-31 15:26:01 +0100

    Finished exercise & change README.md

--- js/main.js
+++ js/main.js
@@ -1,26 +1,69 @@
-// Object constructor
+// Objects constructor
 function warrior(name, attackValue, healthValue) {
   this.name = name,
   this.attackValue = attackValue,
   this.healthValue = healthValue,
   this.attack = function(enemy) {
     enemy.healthValue -= this.attackValue;
-    alert(this.name + " attacked " + enemy.name + " ! He stays " + enemy.healthValue + " life points to " + enemy.name + " !");
 
     if (enemy.healthValue <= 0) {
-      alert(enemy.name + " is dead...");
+      alert(this.name + " attacked " + enemy.name + " ! " + enemy.name + " is dead...");
     } else if (this.healthValue <= 0) {
-      alert(this.name + " is dead...");
+      alert(this.name + " attacked " + enemy.name + " ! " + this.name + " is dead...");
     } else if (this.healthValue > 0 && enemy.healthValue > 0) {
-      enemy.attack(this);
+      alert(this.name + " attacked " + enemy.name + " ! He stays " + enemy.healthValue + " life points to " + enemy.name + " !");
+      if (this.name === "Bell") {
+        enemy.attack(goblin);
+      } else if (this.name === "Ugly") {
+        enemy.attack(pixie);
+      }
+
     }
 
   }
 } // End of object warrior()
 
+function magician(name, attackValue, healthValue, magicValue) {
+  this.name = name,
+  this.attackValue = attackValue,
+  this.healthValue = healthValue,
+  this.magicValue = magicValue,
+  this.attack = function(enemy) {
+    enemy.healthValue -= this.attackValue;
+
+    if (enemy.healthValue <= 0) {
+      alert(this.name + " attacked " + enemy.name + " ! " + enemy.name + " is dead...");
+    } else if (this.healthValue <= 0) {
+      alert(this.name + " attacked " + enemy.name + " ! " + this.name + " is dead...");
+    } else if (this.healthValue > 0 && enemy.healthValue > 0) {
+
+      if (this.healthValue <= 75 && this.magicValue >= 20) {
+        this.healing();
+        alert("Healing: " + this.name + " has " + this.healthValue + " life points !");
+      } else if (this.magicValue < 20) {
+        alert("Healing impossible: " + this.name + " has no more life points ! ");
+      }
+
+      alert(this.name + " attacked " + enemy.name + " ! He stays " + enemy.healthValue + " life points to " + enemy.name + " !");
+      enemy.attack(this);
+    }
+
+  },
+  this.healing = function() {
+    this.magicValue -= 20;
+    this.healthValue += 10;
+  }
+} // End of object magician()
+
+
 // Create character
-var pixie = new warrior("Bell", 25, 100);
-var goblin = new warrior("Ugly", 40, 50);
+var pixie = new warrior("Bell", 25, 120);
+var goblin = new warrior("Ugly", 40, 100);
+var wizard = new magician("Lazy", 30, 150, 100);
 
 // Launch the fight
-pixie.attack(goblin);
+wizard.attack(pixie);
+
+
+
+//
